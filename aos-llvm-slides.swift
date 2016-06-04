3b0d6a90cc829a40b6f4e1fc4369ba26293e71b7
Yan Li <eyeplum@gmail.com>
2016-06-04 21:22:50 +0800

    Initial commit

---
+++ aos-llvm-slides.swift
@@ -0,0 +1,56 @@
+let slides = Slides(pages: [
+
+  Cover(title: "The Architecture of LLVM", bulletPoints: ["yan.li@ef.com"]),  
+
+  Page(title: "What is LLVM?", bulletPoints: [
+    "The name LLVM was once an acronym for:",
+    "  Low Level Virtual Machine",
+    "Now, LLVM is an umbrella project for:",
+    "  llvm - the compiler",
+    "  clang - the C family frontend",
+    "  lldb - the debugger",
+    "  libc++ - the C++ STL implementation",
+    "  lld - the linker",
+    "  and more..."
+  ]),
+
+  Page(title: "The Three-Phase Compiler Design", bulletPoints: [
+    "placeholder",
+  ]),
+
+  Page(title: "The GNU C Compiler", bulletPoints: [
+    "placeholder",
+  ]),
+
+  Page(title: "LLVM IR", bulletPoints: [
+    "placeholder",
+  ]),
+
+  Page(title: "LLVM's Three-Phase Compiler Design", bulletPoints: [
+    "placeholder",
+  ]),
+
+  Page(title: "The LLVM Code Generator", bulletPoints: [
+    "placeholder",
+  ]),
+
+  Page(title: "Extra Benefits of the Design", bulletPoints: [
+    "placeholder",
+  ]),
+
+  Page(title: "Future Directions", bulletPoints: [
+    "placeholder",
+  ]),
+
+  Page(title: "Swift and LLVM", bulletPoints: [
+    "placeholder",
+  ]),
+
+  Page(title: "Use LLVM as a Library", bulletPoints: [
+    "placeholder",
+  ]),
+
+  Cover(title: "Who's next?", bulletPoints: []),
+
+])
+
