289ac0c10dc56492ba83cfb4cf2c6119385e727c
Yan Li <eyeplum@gmail.com>
2016-06-05 22:11:22 +0800

    Update contents

--- aos-llvm-slides.swift
+++ aos-llvm-slides.swift
@@ -2,7 +2,7 @@ let slides = Slides(pages: [
 
   Cover(title: "The Architecture of LLVM", bulletPoints: ["yan.li@ef.com"]),  
 
-  Image(title: "LLVM-logo.png", bulletPoints: [
+  Image(title: "images/LLVM-logo.png", bulletPoints: [
     "竜神の剣を喰らえ",
     "Ryūjin no ken o kurae",
   ]),
@@ -10,21 +10,43 @@ let slides = Slides(pages: [
   Page(title: "What is LLVM?", bulletPoints: [
     "The name LLVM was once an acronym for:",
     "  Low Level Virtual Machine",
+    "Begins in December 2000",
+    "  Designed as a set of reusable libs...",
+    "  ...with well-defined interfaces",
     "Now, LLVM is an umbrella project for:",
     "  llvm - the compiler",
     "  clang - the C family frontend",
     "  lldb - the debugger",
     "  libc++ - the C++ STL implementation",
     "  lld - the linker",
-    "  and more..."
+    "  and more...",
   ]),
 
-  Page(title: "The Three-Phase Compiler Design", bulletPoints: [
-    "placeholder",
+  Page(title: "Classical Compiler Design", bulletPoints: [
+    "Frontend",
+    "  Source Code -> Lexer -> Token",
+    "  Token -> Parser -> Abstract Syntax Tree",
+    "Optimizer",
+    "  AST -> Optimizer -> AST",
+    "Backend",
+    "  AST -> Code Generator -> Target Instructions",
   ]),
 
-  Page(title: "The GNU C Compiler", bulletPoints: [
-    "placeholder",
+  Image(title: "images/RetargetableCompiler.png", bulletPoints: ["Retargetablity"]),
+
+  Page(title: "Before LLVM", bulletPoints: [
+    "The benifits of a three-phase design was never fully realized",
+    "Java and .NET virtual machines",
+    "  JIT compiler + a runtime + a bytecode format as media",
+    "  Enforces JIT compilation and GC, leads to subopimal performance",
+    "Translate source code to C",
+    "  Perhaps the most unfortunate, but also most popular way",
+    "  Source Code -> C Code -> C Compiler",
+    "  Poor debugging, slow compilation, feature constrained by C",
+    "GNU C Compiler (GCC)",
+    "  Supports many frontends and backends",
+    "  GCC can only be used as a whole",
+    "  This is due to some architectural problems from its early design",
   ]),
 
   Page(title: "LLVM IR", bulletPoints: [
